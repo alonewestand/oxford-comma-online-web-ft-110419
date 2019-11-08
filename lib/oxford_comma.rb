37e83b4a218254c07b83bbaf2667e4729bb84f7a
Ken Goad <goadke@g.cofc.edu>
2019-11-08 02:42:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,38 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
     array[-1].insert(0, "and ")
   end
   array.join(", ")    
+=======
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
+>>>>>>> 8d19e80c543f96bcf6a976733bf393979b6cc77c
 end
